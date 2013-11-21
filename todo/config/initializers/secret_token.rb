20ec663bb977e88728777e6d1cb91e841ba31689
cesar <cesarhenrique37@gmail.com>
2013-11-21 16:53:15 +0000

    bootsratp file e app com utilizadores

---
+++ todo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todo::Application.config.secret_key_base = '471863ec395ada140321fee23e51df90a39544919f9a5519bb183eb74014a91e6726533b99efe0fe02b44ceb0f17a6c12be3b1d90e31906ac7d617b29382ec32'
